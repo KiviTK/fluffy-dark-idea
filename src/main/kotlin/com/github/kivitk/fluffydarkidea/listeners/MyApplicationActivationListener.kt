9f0351f39608c87d7c9cf85de44f2ca17f7dcbdc
GitHub Action <action@github.com>
2024-04-30 11:46:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/kivitk/fluffydarkidea/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/kivitk/fluffydarkidea/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.kivitk.fluffydarkidea.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
